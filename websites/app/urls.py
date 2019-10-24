15309875e3d6924e8bfcaa1a659090d4bdd21463
huyhoang1996ha <huyhoang1996ha@gmail.com>
2019-10-24 22:51:40 +0700

    Add pages

--- websites/app/urls.py
+++ websites/app/urls.py
@@ -3,7 +3,9 @@ import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-
+    url(r'^news/$', views.new, name='new'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^about/$', views.about, name='about'),
 
 ]
     
